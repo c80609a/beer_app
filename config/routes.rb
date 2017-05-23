61b4b1a06a315f9b7ddc351bf8c90a5bdaed349e
C80609A <c080609a@gmail.com>
2017-05-23 23:00:06 +0300

    Добавил маршрут 'beer/:beer_type'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'beer/:beer_id', :to => 'beers#show'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'beer/:beer_type' => 'beers#show'
+
 end
